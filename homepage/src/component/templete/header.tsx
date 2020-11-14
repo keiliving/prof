1a8b937451724f585d653e4fe92eb8407c1078db
keiliving <keiliving6@gmail.com>
2020-11-14 17:40:53 +0900

    footer

---
+++ homepage/src/component/templete/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Created by keiliving in 2020</p>
+    </div>
+  )
+}
+
+export default Footer
