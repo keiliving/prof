fd38a55e30f13ccbdcf2af4342bc1dee1ba07d32
keiliving <keiliving6@gmail.com>
2020-11-22 10:20:23 +0900

    add_tabs

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -2,28 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import Footer from './component/templete/footer'
 import Profile from './component/templete/sidebar'
+import Tabs from './component/templete/tabs'
+import './index.css';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="main-content">
+        <Tabs />
+        
+        <div className="side-bar">
+          <Profile />
+        </div>
+      </div>
       <Footer />
-      <Profile />
     </div>
   );
 }
--- homepage/src/component/templete/sidebar.tsx
+++ homepage/src/component/templete/sidebar.tsx
@@ -1,26 +1,10 @@
 import React, { FC } from 'react';
-import Link from '@material-ui/core/Link';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import TwitterIcon from '@material-ui/icons/Twitter';
-// todo
-// soft-plofile
-// github link
-// qiita link
+import Profile from './profile'
 
-const Profile:FC = () => {
+const Sidebar:FC = () => {
   return (
-    <div className="profile">
-      
-        <Link href="https://github.com/keiliving" target="_blank">
-          <GitHubIcon style={{ fontSize: 20, color: "lightgray", marginRight: 20 }} />
-        </Link>
-
-        <Link href="https://twitter.com/keiliving" target="_blank">
-          <TwitterIcon style={{ fontSize: 20, color: "lightgray" }} />
-        </Link>
-
-    </div>
+    <Profile />
   )
 }
 
-export default Profile
+export default Sidebar
