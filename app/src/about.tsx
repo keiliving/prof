de98e5ad44936fc744a3ac74d0de0497423b548a
keiliving <keiliving6@gmail.com>
2021-08-28 10:38:39 +0900

    fix

---
+++ app/src/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import './about.css';
+
+const About: React.FC<any> = () => (
+  <div className="aboutcontainer">
+    <Typography variant="h4">About</Typography>
+    <p>名前: kei</p>
+  </div>
+);
+
+export default About;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Header from './header';
 import './header.css';
+import About from './about';
 
 export default function Main() {
   return (
@@ -13,16 +13,7 @@ export default function Main() {
       <Header className="header" />
       <Toolbar />
       <Container>
-        <Box my={2}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
+        <About />
       </Container>
     </React.Fragment>
   );
